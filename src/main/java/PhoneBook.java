bb9d9ccbddb775d824fa175b87890a12049b6003
Шалагинов Виталий <v.a.sh@mail.ru>
2022-11-13 02:37:14 +0700

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,9 @@
+import java.util.Map;
+import java.util.TreeMap;
+
 public class PhoneBook {
+    private static Map <String, String> book = new TreeMap<>();
+    public boolean add(String name, String number){
+        return false;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class PhoneBookTest {
+
+    @Test
+    public void testAdd() {
+        PhoneBook phoneBook = new PhoneBook();
+        String name = "Vasya";
+        String number = "+7 912 345 67 89";
+        Assertions.assertTrue(phoneBook.add(name, number));
+    }
 }
