d0a03d7b30359ae989e707b7d4874ef4df3426dc
Шалагинов Виталий <v.a.sh@mail.ru>
2022-11-13 02:38:39 +0700

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -4,6 +4,6 @@ import java.util.TreeMap;
 public class PhoneBook {
     private static Map <String, String> book = new TreeMap<>();
     public boolean add(String name, String number){
-        return false;
+        return book.putIfAbsent(name, number) == null;
     }
 }
